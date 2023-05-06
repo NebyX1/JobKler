edeef192e4ec449ef5d9f6665f18384a2aa89375
NebyX1 <blackcat_neme@hotmail.com>
2023-05-06 09:23:19 -0300

    Modificación de un lote de archivos

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from flask import Flask, render_template, request, redirect, url_for, flash
+from database import load_jobs_from_db as get_all_jobs, load_job_from_db as get_job_by_id, add_application_to_db
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
+@app.route('/home')
 def home():
-    jobs = load_jobs_from_db()
-    return render_template('home.html', Jobs = jobs)
+    jobs = get_all_jobs()
+    return render_template('home.html', jobs=jobs)
+
+@app.route('/job/<int:id>')
+def job_detail(id):
+    job = get_job_by_id(id)
+    return render_template('jobpage.html', job=job)
+
+@app.route('/job/<int:id>/apply', methods=['GET', 'POST'])
+def apply_to_job(id):
+    if request.method == 'POST':
+        full_name = request.form.get('name', '').strip() or None
+        email = request.form.get('email', '').strip() or None
+        linkedin_url = request.form.get('linkedin', '').strip() or None
+        education = request.form.get('education', '').strip() or None
+        work_experience = request.form.get('work_experience', '').strip() or None
+
+        add_application_to_db(id, full_name, email, linkedin_url, education, work_experience)
+
+        return redirect(url_for('home'))
+    else:
+        return render_template('components/application_form.html', job_id=id)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3500, debug=True)
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -2,11 +2,9 @@ import certifi
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
-
-
+from datetime import datetime
 
 load_dotenv()
-
 
 connection_string = os.environ['DB_CONNECTION']
 
@@ -34,3 +32,20 @@ def load_jobs_from_db():
         }
         jobs.append(job)
     return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {'val': id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            row = rows[0]
+            job = {column: value for column, value in zip(result.keys(), row)} 
+            return job
+        
+def add_application_to_db(job_id, full_name, email, linkedin_url, education, work_experience):
+    with engine.connect() as conn:
+        current_time = datetime.utcnow()
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, created_at, updated_at) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :created_at, :updated_at)")
+        conn.execute(query.bindparams(job_id=job_id, full_name=full_name, email=email, linkedin_url=linkedin_url, education=education, work_experience=work_experience, created_at=current_time, updated_at=current_time))
